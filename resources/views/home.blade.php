28622ca35fcf95c9bf074615186053d588ec2b39
dennys willian da silva <dennys_1994@live.com>
2024-11-20 09:23:19 -0300

    Set up a fresh Laravel app

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'Home - Autoescola')
+
+@section('content')
+    <h1>Bem-vindo à Autoescola</h1>
+    <p>Conteúdo da página inicial.</p>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::view('/', 'home')->name('home');
+Route::view('/a-escola', 'a_escola')->name('a_escola');
+Route::view('/primeira-habilitacao', 'primeira_habilitacao')->name('primeira_habilitacao');
+Route::view('/inclusao-de-categoria', 'inclusao_categoria')->name('inclusao_categoria');
+Route::view('/reciclagem', 'reciclagem')->name('reciclagem');
+Route::view('/avaliacao', 'avaliacao')->name('avaliacao');
+Route::view('/pre-matricula', 'pre_matricula')->name('pre_matricula');
+Route::view('/contato', 'contato')->name('contato');
+Route::view('/area-do-aluno', 'area_aluno')->name('area_aluno');
